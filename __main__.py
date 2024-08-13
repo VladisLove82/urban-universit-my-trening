b7518e558057fc70b361b6be81d03f0cd4d555eb
VladisLove82 <zloe.moloco@gmail.com>
2024-08-13 22:51:56 +0300

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,14 @@
+name="Топинамбур"
+print(name[0])
+
+name='Топинамбур'
+print(name [-1])
+
+name='Топинамбур'
+print(name[5:])
+
+name = "Топинамбур"
+print(name[::-1])
+
+name = "Топинамбур"
+print(name[1:10:2])
