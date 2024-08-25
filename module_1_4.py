e132eea7cc0bac685a163afa8db3c21912e66022
VladisLove82 <zloe.moloco@gmail.com>
2024-08-25 20:23:44 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Urban')
+print(len(my_string))
+print("my_string".upper())
+print('my_string'.lower())
+print('my_string'.replace('_',''))
+name = 'my_string'
+print (name[0])
+print (name[-1])
