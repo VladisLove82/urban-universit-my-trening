9d682b56daf07063a13e0ff6cbd1eae174c5915b
VladisLove82 <zloe.moloco@gmail.com>
2024-08-26 21:46:38 +0300

    Add files via upload

---
+++ mane.py
@@ -0,0 +1,26 @@
+# 1st program
+a =  9
+b = 0.5
+c = 5
+print((a**b) * c)
+
+#2nd program
+print(9.99 > 9.98, 1000 != 1000.1)
+
+#3st program
+a = (2 + 2) *2
+b = 2 + 2 * 2
+print(a == b)
+
+#4th program
+number = "123.456"
+print(number[4:5])
+
+number_string = '123.456'
+float_number = float(number_string)
+result = float_number * 10
+print(result)
+number_string = 1234.56
+float_number = int(number_string)
+result = float_number / 10
+print(result)
